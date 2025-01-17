fa9ac6a6f55db42c84cf6c33d69d9804b42eefb1
Beyzaerdd <erdbeyza14@gmail.com>
2025-01-18 00:56:33 +0300

    ✅CategoryDTO is complated

--- Backend/ECommerce.Entity/Concrete/Category.cs
+++ Backend/ECommerce.Entity/Concrete/Category.cs
@@ -12,6 +12,7 @@ namespace ECommerce.Entity.Concrete
     {
        
         public string Name { get; set; }
+        public string Description { get; set; }
         public int? ParentCategoryId { get; set; }
         public Category ParentCategory { get; set; }
         public ICollection<Category> SubCategories { get; set; } 
