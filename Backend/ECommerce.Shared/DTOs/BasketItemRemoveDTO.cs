62b9a4f4ed157f78be6daf52ca5450cbc7310855
Beyzaerdd <erdbeyza14@gmail.com>
2025-01-18 00:50:52 +0300

    ✅BasketItemRemoveDTO is complated

---
+++ Backend/ECommerce.Shared/DTOs/BasketItemRemoveDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Shared.DTOs
+{
+    public class BasketItemRemoveDTO
+    {
+        public int BasketId { get; set; }
+        public int ProductId { get; set; }
+    }
+}
