16f1cf64bf7d4c7c9c749d17ad2879f273169e76
Beyzaerdd <erdbeyza14@gmail.com>
2025-01-18 01:10:55 +0300

    ✅OrderCreateDTO is complated

---
+++ Backend/ECommerce.Shared/DTOs/OrderCreateDTO.cs
@@ -0,0 +1,19 @@
+﻿using ECommerce.Shared.ComplexTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Shared.DTOs
+{
+    public class OrderCreateDTO
+    {
+        public string ApplicationUserId { get; set; }
+    
+        public DateTime OrderDate { get; set; }
+        public IEnumerable<OrderItemCreateDTO> OrderItems { get; set; } = new List<OrderItemCreateDTO>();
+        public InvoiceCreateDTO Invoice { get; set; }
+        public decimal TotalAmount => OrderItems.Sum(item => item.UnitPrice * item.Quantity);
+    }
+}
