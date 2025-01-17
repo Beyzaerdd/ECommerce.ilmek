d53c73c7e8e0c2765e62d3f21c06f533ae47b80f
Beyzaerdd <erdbeyza14@gmail.com>
2025-01-17 23:41:10 +0300

    ✅BasketDto completed

---
+++ Backend/ECommerce.Shared/DTOs/BasketDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Shared.DTOs
+{
+    public  class BasketDTO
+    {
+        public string ApplicationUserId { get; set; }
+        public ApplicationUserDTO ApplicationUser { get; set; }
+
+
+        public ICollection<BasketItemDTO> BasketItems { get; set; }
+    }
+}
