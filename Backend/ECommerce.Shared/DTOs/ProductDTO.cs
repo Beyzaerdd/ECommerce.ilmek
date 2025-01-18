f85b17401c52a9be9592ddff9bdbcb2de6c07ed5
Beyzaerdd <erdbeyza14@gmail.com>
2025-01-18 22:54:52 +0300

    ✅ProductDTO is complated

---
+++ Backend/ECommerce.Shared/DTOs/ProductDTO.cs
@@ -0,0 +1,25 @@
+﻿using ECommerce.Shared.ComplexTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Shared.DTOs
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; } 
+        public string Description { get; set; }
+        public decimal UnitPrice { get; set; } 
+        public ProductSize Size { get; set; } 
+        public ProductColor Color { get; set; } 
+        public int PreparationTimeInDays { get; set; }
+        public bool IsActive { get; set; } 
+        public string CategoryName { get; set; } 
+        public string SubcategoryName { get; set; }
+        public string ImageUrl { get; set; } 
+        public IEnumerable<DiscountDTO> Discounts { get; set; }
+    }
+}
