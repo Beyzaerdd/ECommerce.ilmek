4b3d147f76b6d54a9389a4701ec2119c6057b435
Beyzaerdd <erdbeyza14@gmail.com>
2025-01-18 23:11:35 +0300

    ✅DiscountDTO is complated

---
+++ Backend/ECommerce.Shared/DTOs/DiscountDTO.cs
@@ -0,0 +1,23 @@
+﻿using ECommerce.Shared.ComplexTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Shared.DTOs
+{
+    public class DiscountDTO
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; } 
+        public decimal DiscountValue { get; set; }
+        public DiscountType Type { get; set; } 
+        public string CouponCode { get; set; } 
+        public string ApplicationUserId { get; set; }
+        public string ApplicationUserName { get; set; } 
+        public DateTime StartDate { get; set; } 
+        public DateTime EndDate { get; set; } 
+        public bool IsActive { get; set; }
+    }
+}
