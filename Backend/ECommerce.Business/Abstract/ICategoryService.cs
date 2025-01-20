d369ca9b3e367b26bd2d9eb95eec2334e4f71c98
Beyzaerdd <erdbeyza14@gmail.com>
2025-01-20 22:51:26 +0300

    ✅NoContentDTO is created

---
+++ Backend/ECommerce.Business/Abstract/ICategoryService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Business.Abstract
+{
+    public class ICategoryService
+    {
+        public interface ICategoryService
+        {
+           
+            Task<ResponseDTO<CategoryDTO>> AddCategoryAsync(CategoryCreateDTO categoryCreateDTO);
+            Task<ResponseDTO<NoContent>> UpdateCategoryAsync(CategoryUpdateDTO categoryUpdateDTO);
+            Task<ResponseDTO<NoContent>> SoftDeleteCategoryAsync(int id); // Soft delete
+            Task<ResponseDTO<NoContent>> HardDeleteCategoryAsync(int id); // Hard delete
+
+        
+            Task<ResponseDTO<CategoryDTO>> GetCategoryByIdAsync(int id);
+            Task<ResponseDTO<IEnumerable<CategoryDTO>>> GetAllCategoriesAsync();
+            Task<ResponseDTO<IEnumerable<CategoryDTO>>> GetCategoriesByParentIdAsync(int? parentId);
+
+         
+            Task<ResponseDTO<int>> GetCategoryCountAsync();
+            Task<ResponseDTO<bool>> AnyCategoryAsync(Expression<Func<Category, bool>> predicate);
+        }
+
+    }
+}
