bb824f7aca7a1a6698a0a5c00a2ea0fe22180043
Beyzaerdd <erdbeyza14@gmail.com>
2025-01-21 16:01:01 +0300

    ✅MappingProfile is created

--- Backend/ECommerce.Business/Concrete/CategoryService.cs
+++ Backend/ECommerce.Business/Concrete/CategoryService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ECommerce.Data.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,8 @@ namespace ECommerce.Business.Concrete
 {
     public class CategoryService
     {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IMapper
+
     }
 }
---
+++ Backend/ECommerce.Business/Mapping/MappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using ECommerce.Entity.Concrete;
+using ECommerce.Shared.DTOs.CategoryDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Business.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            #region Category
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+            CreateMap<Category, CategoryCreateDTO>().ReverseMap();
+            CreateMap<Category, CategoryUpdateDTO>().ReverseMap();
+
+            #endregion
+        }
+    }
+}
