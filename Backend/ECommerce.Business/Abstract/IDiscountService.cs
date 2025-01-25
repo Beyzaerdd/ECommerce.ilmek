b4d5962de2a60d5ba4b48a1b73a2a42d81401f60
Beyzaerdd <erdbeyza14@gmail.com>
2025-01-26 02:18:42 +0300

    ✅IDiscountService is Complated

---
+++ Backend/ECommerce.Business/Abstract/IDiscountService.cs
@@ -0,0 +1,24 @@
+﻿using ECommerce.Shared.DTOs.DiscountDTOs;
+using ECommerce.Shared.DTOs.ResponseDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Business.Abstract
+{
+    public interface IDiscountService
+    {
+        
+    
+            Task<ResponseDTO<DiscountDTO>> UpdateDiscountAsync(DiscountUptadeDTO discountUpdateDTO);
+            Task<ResponseDTO<bool>> DeleteDiscountAsync(int discountId);
+            Task<ResponseDTO<List<DiscountDTO>>> GetAllDiscountsAsync();
+            Task<ResponseDTO<DiscountDTO>> GetDiscountByIdAsync(int discountId);
+            Task<ResponseDTO<DiscountDTO>> CreateProductDiscountAsync(DiscountCreateDTO discountCreateDTO);
+            Task<ResponseDTO<DiscountDTO>> CreateCouponCodeAsync(DiscountCreateDTO discountCreateDTO);
+        
+
+    }
+}
