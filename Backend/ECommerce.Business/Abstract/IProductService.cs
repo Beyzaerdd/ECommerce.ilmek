6c1f5c60ee27454d26a1cf50d1500832635edef8
Beyzaerdd <erdbeyza14@gmail.com>
2025-01-25 18:09:24 +0300

    ✅Image service added

--- Backend/ECommerce.Business/Abstract/IProductService.cs
+++ Backend/ECommerce.Business/Abstract/IProductService.cs
@@ -16,7 +16,7 @@ namespace ECommerce.Business.Abstract
         Task<ResponseDTO<NoContent>> HardDeleteProductAsync(int id);
 
         Task<ResponseDTO<ProductDTO>> GetProductByIdAsync(int id);
-        Task<ResponseDTO<IEnumerable<ProductDTO>>> GetAllProductsAsync();
+        Task<ResponseDTO<IEnumerable<ProductDTO>>> GetAllProductsAsync( int? take = null);
 
         Task<ResponseDTO<IEnumerable<ProductDTO>>> GetProductsWithCategoriesAsync(int parentCategoryId);
         Task<ResponseDTO<IEnumerable<ProductDTO>>> GetProductsBySubCategoryIdAsync(int subCategoryId);
