c1dc179b0e35f476bee32961a71093b24c812b00
Beyzaerdd <erdbeyza14@gmail.com>
2025-01-26 02:16:57 +0300

    ✅ Added Model binding attributes and [Authorize(Policy)

--- Backend/ECommerce.API/Controllers/CategoriesController.cs
+++ Backend/ECommerce.API/Controllers/CategoriesController.cs
@@ -18,26 +18,27 @@ namespace ECommerce.API.Controllers
         {
             _categoryService = categoryService;
         }
-
-        [HttpPost]
         [Authorize(Policy = "SellerAndAdmin")]
-        public async Task<IActionResult> CreateCategory(CategoryCreateDTO categoryCreateDTO)
+        [HttpPost]
+        
+        public async Task<IActionResult> CreateCategory([FromBody] CategoryCreateDTO categoryCreateDTO)
         {
             var response = await _categoryService.AddCategoryAsync(categoryCreateDTO);
             return CreateResponse(response);
         }
-
-        [HttpGet("getall")]
         [Authorize(Policy = "User")]
+        [HttpGet("getall")]
+        
         public async Task<IActionResult> GetAllCategories()
         {
             var response = await _categoryService.GetAllCategoriesAsync();
             return CreateResponse(response);
         }
 
-        [HttpGet("{id}")]
         [Authorize(Policy = "User")]
-        public async Task<IActionResult> GetCategoryById(int id)
+        [HttpGet("{id}")]
+       
+        public async Task<IActionResult> GetCategoryById([FromRoute] int id)
         {
             var response = await _categoryService.GetCategoryByIdAsync(id);
             return CreateResponse(response);
@@ -45,15 +46,15 @@ namespace ECommerce.API.Controllers
 
         [HttpGet("getByParentId")]
         [Authorize(Policy = "User")]
-        public async Task<IActionResult> GetCategoriesByParentId(int? parentId)
+        public async Task<IActionResult> GetCategoriesByParentId([FromQuery] int? parentId)
         {
             var response = await _categoryService.GetCategoriesByParentIdAsync(parentId);
             return CreateResponse(response);
         }
-
-        [HttpPut]
         [Authorize(Policy = "Admin")]
-        public async Task<IActionResult> UpdateCategory(CategoryUpdateDTO categoryUpdateDTO)
+        [HttpPut]
+     
+        public async Task<IActionResult> UpdateCategory([FromBody] CategoryUpdateDTO categoryUpdateDTO)
         {
             var response = await _categoryService.UpdateCategoryAsync(categoryUpdateDTO);
             return CreateResponse(response);
@@ -62,21 +63,21 @@ namespace ECommerce.API.Controllers
         }
         [Authorize(Policy = "Admin")]
         [HttpDelete("softDelete/{id}")]
-        public async Task<IActionResult> SoftDeleteCategory(int id)
+        public async Task<IActionResult> SoftDeleteCategory([FromRoute] int id)
         {
             var response = await _categoryService.SoftDeleteCategoryAsync(id);
             return CreateResponse(response);
         }
         [Authorize(Policy = "Admin")]
         [HttpDelete("hardDelete/{id}")]
-        public async Task<IActionResult> HardDeleteCategory(int id)
+        public async Task<IActionResult> HardDeleteCategory([FromRoute] int id)
         {
             await _categoryService.HardDeleteCategoryAsync(id);
             return NoContent();
         }
         [Authorize(Policy = "Admin")]
         [HttpGet("count/{isActive?}")]
-        public async Task<IActionResult> GetCategoryCount(bool? isActive)
+        public async Task<IActionResult> GetCategoryCount([FromQuery] bool? isActive)
         {
             var response = await _categoryService.GetCategoryCountAsync(isActive);
             return CreateResponse(response);
--- Backend/ECommerce.API/Controllers/ProductsController.cs
+++ Backend/ECommerce.API/Controllers/ProductsController.cs
@@ -17,94 +17,115 @@ namespace ECommerce.API.Controllers
         {
             _productService = productService;
         }
+
+        [Authorize(Policy = "SellerAndAdmin")]
         [HttpPost("AddProduct")]
-        public async Task<IActionResult> CreateProduct(ProductCreateDTO productCreateDTO)
+        public async Task<IActionResult> CreateProduct([FromBody] ProductCreateDTO productCreateDTO)
         {
             var response = await _productService.AddProductAsync(productCreateDTO);
             return CreateResponse(response);
         }
-        [HttpPut]
+
         [Authorize(Policy = "SellerAndAdmin")]
-        public async Task<IActionResult> UpdateProduct(ProductUpdateDTO productUpdateDTO)
+        [HttpPut]
+        
+        public async Task<IActionResult> UpdateProduct([FromBody] ProductUpdateDTO productUpdateDTO)
         {
             var response = await _productService.UpdateProductAsync(productUpdateDTO);
             return CreateResponse(response);
         }
-        [HttpDelete("softDelete/{id}")]
+
         [Authorize(Policy = "SellerAndAdmin")]
-        public async Task<IActionResult> SoftDeleteProduct(int id)
+        [HttpDelete("softDelete/{id}")]
+       
+        public async Task<IActionResult> SoftDeleteProduct([FromRoute] int id)
         {
             var response = await _productService.SoftDeleteProductAsync(id);
             return CreateResponse(response);
         }
-        [HttpDelete("hardDelete/{id}")]
+
         [Authorize(Policy = "SellerAndAdmin")]
-        public async Task<IActionResult> HardDeleteProduct(int id)
+        [HttpDelete("hardDelete/{id}")]
+       
+        public async Task<IActionResult> HardDeleteProduct( [FromRoute] int id)
         {
             var response = await _productService.HardDeleteProductAsync(id);
             return CreateResponse(response);
         }
-        [HttpGet("GetProductBy/{id}")]
+
         [Authorize(Policy = "User")]
-        public async Task<IActionResult> GetProductById(int id)
+        [HttpGet("GetProductBy/{id}")]
+   
+        public async Task<IActionResult> GetProductById([FromRoute] int id)
         {
             var response = await _productService.GetProductByIdAsync(id);
             return CreateResponse(response);
         }
-        [HttpGet("getall")]
+
+
         [Authorize(Policy = "User")]
+        [HttpGet("getall")]
+    
         public async Task<IActionResult> GetAllProducts()
         {
             var response = await _productService.GetAllProductsAsync();
             return CreateResponse(response);
         }
-        [HttpGet("getByCategory/{categoryId}")]
         [Authorize(Policy = "User")]
-        public async Task<IActionResult> GetProductsByCategory(int categoryId)
+        [HttpGet("getByCategory/{categoryId}")]
+      
+        public async Task<IActionResult> GetProductsByCategory([FromRoute] int categoryId)
         {
             var response = await _productService.GetProductsWithCategoriesAsync(categoryId);
             return CreateResponse(response);
         }
-        [HttpGet("getBySubCategory/{subCategoryId}")]
         [Authorize(Policy = "User")]
-        public async Task<IActionResult> GetProductsBySubCategory(int subCategoryId)
+        [HttpGet("getBySubCategory/{subCategoryId}")]
+       
+        public async Task<IActionResult> GetProductsBySubCategory([FromRoute] int subCategoryId)
         {
             var response = await _productService.GetProductsBySubCategoryIdAsync(subCategoryId);
             return CreateResponse(response);
         }
-        [HttpGet("getCount/{isActive}")]
         [Authorize(Policy = "SellerAndAdmin")]
-        public async Task<IActionResult> GetProductCount(bool? isActive)
+        [HttpGet("getCount/{isActive}")]
+       
+        public async Task<IActionResult> GetProductCount([FromQuery] bool? isActive)
         {
             var response = await _productService.GetProductCountAsync(isActive);
             return CreateResponse(response);
         }
-        [HttpGet("getCountBySubCategory/{subCategoryId}")]
+
         [Authorize(Policy = "SellerAndAdmin")]
-        public async Task<IActionResult> GetCountByCategory(int subCategoryId)
+        [HttpGet("getCountBySubCategory/{subCategoryId}")]
+
+        public async Task<IActionResult> GetCountByCategory([FromRoute] int subCategoryId)
         {
             var response = await _productService.GetCountBySubCategory(subCategoryId);
             return CreateResponse(response);
         }
 
-        [HttpGet("getByColor/{productColor}")]
+
         [Authorize(Policy = "User")]
-        public async Task<IActionResult> GetProductByColor(int productColor)
+        [HttpGet("getByColor/{productColor}")]
+
+        public async Task<IActionResult> GetProductByColor([FromQuery] int productColor)
         {
             var response = await _productService.GetProductByColor(productColor);
             return CreateResponse(response);
         }
-        [HttpGet("getByPriceRange/{minPrice}/{maxPrice}")]
         [Authorize(Policy = "User")]
-        public async Task<IActionResult> GetProductByPriceRange(decimal minPrice, decimal maxPrice)
+        [HttpGet("getByPriceRange/{minPrice}/{maxPrice}")]
+        
+        public async Task<IActionResult> GetProductByPriceRange([FromQuery] decimal minPrice, [FromQuery] decimal maxPrice)
         {
             var response = await _productService.GetProductByPriceRange(minPrice, maxPrice);
             return CreateResponse(response);
         }
-
-        [HttpGet("getBySize/{productSize}")]
         [Authorize(Policy = "User")]
-        public async Task<IActionResult> GetProductBySize(int productSize)
+        [HttpGet("getBySize/{productSize}")]
+      
+        public async Task<IActionResult> GetProductBySize([FromQuery] int productSize)
         {
             var response = await _productService.GetProductBySize(productSize);
             return CreateResponse(response);
