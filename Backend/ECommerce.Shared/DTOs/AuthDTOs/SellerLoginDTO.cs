3dfdf597bf5267e5e39de63a7bb78350be04a789
Beyzaerdd <erdbeyza14@gmail.com>
2025-01-25 22:49:49 +0300

    ✅ AuthDTOs are created

---
+++ Backend/ECommerce.Shared/DTOs/AuthDTOs/SellerLoginDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Shared.DTOs.AuthDTOs
+{
+    public class SellerLoginDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+       
+
+    }
+}
---
+++ Backend/ECommerce.Shared/DTOs/AuthDTOs/SellerRegisterDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Shared.DTOs.AuthDTOs
+{
+    public class SellerRegisterDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string PhoneNumber { get; set; }
+        public string PostalCode { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public int IdentityNumber { get; set; }
+        public string StoreName { get; set; }
+        public string Role { get; set; } = "Seller";
+        public bool IsApproved { get; set; } = false;
+    }
+}
