9e678e0c4ec690a038e2d3707f8d7a8d13b369c2
Beyzaerdd <erdbeyza14@gmail.com>
2025-01-25 23:10:08 +0300

    ✅IAuthService is created

---
+++ Backend/ECommerce.Business/Abstract/IAuthService.cs
@@ -0,0 +1,23 @@
+﻿using ECommerce.Shared.DTOs.AuthDTOs;
+using ECommerce.Shared.DTOs.ResponseDTOs;
+using ECommerce.Shared.DTOs.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Business.Abstract
+{
+    public interface IAuthService
+    {
+        Task<ResponseDTO<TokenDTO>> LoginUserAsync(UserLoginDTO userLoginDTO);
+        Task<ResponseDTO<NoContent>> RegisterUserAsync(UserRegisterDTO userRegisterDTO);
+        Task<ResponseDTO<TokenDTO>> LoginSellerAsync(SellerLoginDTO sellerLoginDTO);
+        Task<ResponseDTO<NoContent>> RegisterSellerAsync(SellerRegisterDTO sellerRegisterDTO);
+
+        Task<ResponseDTO<NoContent>> ForgotPasswordAsync(ForgotPasswordDTO forgotPasswordDTO);
+        Task<ResponseDTO<NoContent>> ChangePasswordAsync(ChangePasswordDTO changePasswordDTO);
+
+    }
+}
