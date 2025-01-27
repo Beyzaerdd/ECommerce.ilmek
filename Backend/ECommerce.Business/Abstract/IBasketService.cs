706f9ec86eb9235845ab8e6165e78253a32018c2
Beyzaerdd <erdbeyza14@gmail.com>
2025-01-27 20:56:29 +0300

    ✅IBasketService is complated

---
+++ Backend/ECommerce.Business/Abstract/IBasketService.cs
@@ -0,0 +1,25 @@
+﻿using ECommerce.Shared.DTOs.BasketDTOs;
+using ECommerce.Shared.DTOs.ResponseDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Business.Abstract
+{
+    public interface IBasketService
+    {
+        Task<ResponseDTO<BasketDTO>> GetBasketAsync(string applicationUserId);
+     
+        Task<ResponseDTO<BasketDTO>> CreateBasketAsync(BasketCreateDTO basketCreateDTO);
+        Task<ResponseDTO<NoContent>> ClearBasketAsync(string applicationUserId);
+        Task<ResponseDTO<BasketItemDTO>> AddProductToBasketAsync(BasketItemCreateDTO basketItemCreateDTO);
+        Task<ResponseDTO<NoContent>> RemoveProductFromBasketAsync(int basketItemId);
+        Task<ResponseDTO<NoContent>> ChangeProductQuantityAsync(BasketItemChangeQuantityDTO basketItemChangeQuantityDTO);
+
+       
+        Task<ResponseDTO<decimal>> CalculateTotalAmountAsync(BasketDTO basketDTO);
+
+    }
+}
