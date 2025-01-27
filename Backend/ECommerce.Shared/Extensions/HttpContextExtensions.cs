72889f67dd75469c93ba2d50c2610b4d65809448
Beyzaerdd <erdbeyza14@gmail.com>
2025-01-27 23:56:35 +0300

    ✅HttpContextExtensions is created

---
+++ Backend/ECommerce.Shared/Extensions/HttpContextExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Shared.Extensions
+{
+    public static class HttpContextExtensions
+    {
+        public static string GetUserId(this Microsoft.AspNetCore.Http.IHttpContextAccessor contextAccessor)
+        {
+            return contextAccessor.HttpContext.User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
+        }
+
+    }
+}
