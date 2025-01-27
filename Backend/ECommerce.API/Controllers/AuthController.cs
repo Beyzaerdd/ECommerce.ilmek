bf2809659b74c7d3c48c32179ec922cacab15933
Beyzaerdd <erdbeyza14@gmail.com>
2025-01-28 00:04:14 +0300

    ✅AuthController is complated

---
+++ Backend/ECommerce.API/Controllers/AuthController.cs
@@ -0,0 +1,64 @@
+﻿using ECommerce.Business.Abstract;
+using ECommerce.Shared.DTOs.AuthDTOs;
+using ECommerce.Shared.Helpers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : CustomControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("Userlogin")]
+        public async Task<IActionResult> LoginUser([FromBody] UserLoginDTO userLoginDTO)
+        {
+            var response = await _authService.LoginUserAsync(userLoginDTO);
+            return CreateResponse(response);
+        }
+
+        [HttpPost("UserRegister")]
+        public async Task<IActionResult> RegisterUser([FromBody] UserRegisterDTO userRegisterDTO)
+        {
+            var response = await _authService.RegisterUserAsync(userRegisterDTO);
+            return CreateResponse(response);
+        }
+
+        [HttpPost("SellerLogin")]
+        public async Task<IActionResult> LoginSeller([FromBody] SellerLoginDTO sellerLoginDTO)
+        {
+            var response = await _authService.LoginSellerAsync(sellerLoginDTO);
+            return CreateResponse(response);
+        }
+
+        [HttpPost("SellerRegister")]
+        public async Task<IActionResult> RegisterSeller([FromBody] SellerRegisterDTO sellerRegisterDTO)
+        {
+            var response = await _authService.RegisterSellerAsync(sellerRegisterDTO);
+            return CreateResponse(response);
+        }
+
+        [HttpPost("ForgotPassword")]
+        public async Task<IActionResult> ForgotPassword([FromBody] ForgotPasswordDTO forgotPasswordDTO)
+        {
+            var response = await _authService.ForgotPasswordAsync(forgotPasswordDTO);
+            return CreateResponse(response);
+        }
+
+        [HttpPost("ChangePassword")]
+        public async Task<IActionResult> ChangePassword([FromBody] ChangePasswordDTO changePasswordDTO)
+        {
+            var response = await _authService.ChangePasswordAsync(changePasswordDTO);
+            return CreateResponse(response);
+        }
+
+
+    }
+}
