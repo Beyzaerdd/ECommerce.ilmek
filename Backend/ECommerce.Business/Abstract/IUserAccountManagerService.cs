35b8225d3646f151b25fd6b84e86749c47c8add3
Beyzaerdd <erdbeyza14@gmail.com>
2025-01-30 17:08:10 +0300

    ✅IUserAccountManagerService is complated

---
+++ Backend/ECommerce.Business/Abstract/IUserAccountManagerService.cs
@@ -0,0 +1,24 @@
+﻿using ECommerce.Entity.Concrete;
+using ECommerce.Shared.DTOs.ResponseDTOs;
+using ECommerce.Shared.DTOs.UsersDTO;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Business.Abstract
+{
+    public interface IUserAccountManagerService
+    {
+        Task<ResponseDTO<IEnumerable<ApplicationUserDTO>>> GetPendingSellersAsync();
+        Task<ResponseDTO<ApplicationUserDTO>> ApproveSellerAsync(string sellerId); 
+        Task <ResponseDTO<IEnumerable<ApplicationUserDTO>>> GetAllSellersAsync();
+        Task<ResponseDTO<IEnumerable<ApplicationUserDTO>>> GetAllUsersAsync();
+        Task<ResponseDTO<ApplicationUserDTO>> GetUserByIdAsync(string userId);
+
+        Task<ResponseDTO<ApplicationUserDTO>> GetUserAccountDetails();
+        Task<ResponseDTO<Seller>> GetSellerAccountDetails();
+    }
+}
