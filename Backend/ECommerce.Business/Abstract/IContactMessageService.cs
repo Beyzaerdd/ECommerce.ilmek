8312639d9a6a7de80be9629484af85205e7f6c1f
Beyzaerdd <erdbeyza14@gmail.com>
2025-01-30 17:05:07 +0300

    ✅IContactMessageService is created

---
+++ Backend/ECommerce.Business/Abstract/IContactMessageService.cs
@@ -0,0 +1,18 @@
+﻿using ECommerce.Shared.DTOs.ContactMessageDTOs;
+using ECommerce.Shared.DTOs.ResponseDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Business.Abstract
+{
+    public interface IContactMessageService
+    {
+        Task<ResponseDTO<NoContent>> AddContactMessageAsync(ContactMessageCreateDTO contactMessageCreateDTO);
+        Task<ResponseDTO<IEnumerable<ContactMessageDTO>>> GetContactMessagesAsync();
+        Task<ResponseDTO<IEnumerable<ContactMessageDTO>>> GetContactMessageByUserIdAsync(string applicationUserId);
+        Task<ResponseDTO<NoContent>> DeleteContactMessageAsync(int contactMessageId);
+    }
+}
