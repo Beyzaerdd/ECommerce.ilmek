0577ea23e4a945d0aa56abc5e05a13af5f508e97
Beyzaerdd <erdbeyza14@gmail.com>
2025-02-01 00:49:44 +0300

    ✅IInvoiceService is completed

---
+++ Backend/ECommerce.Business/Abstract/IInvoiceService.cs
@@ -0,0 +1,18 @@
+﻿using ECommerce.Shared.DTOs.InvoiceDTOs;
+using ECommerce.Shared.DTOs.OrderDTOs;
+using ECommerce.Shared.DTOs.ResponseDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Business.Abstract
+{
+    public interface IInvoiceService
+    {
+
+        Task<ResponseDTO<InvoiceDTO>> CreateInvoiceAsync(InvoiceCreateDTO invoiceCreateDTO);
+        Task<ResponseDTO<InvoiceDTO>> GetInvoiceByOrderIdAsync(int orderId);
+    }
+}
