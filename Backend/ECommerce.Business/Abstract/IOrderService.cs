831a2be03ffedf1dd91e1f4025fb3c614e73429f
Beyzaerdd <erdbeyza14@gmail.com>
2025-02-01 00:46:41 +0300

    ✅IOrderService is complated

---
+++ Backend/ECommerce.Business/Abstract/IOrderService.cs
@@ -0,0 +1,29 @@
+﻿using ECommerce.Shared.ComplexTypes;
+using ECommerce.Shared.DTOs.OrderDTOs;
+using ECommerce.Shared.DTOs.ResponseDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Business.Abstract
+{
+    public interface IOrderService
+    {
+        Task<ResponseDTO<OrderDTO>> GetOrderAsync(int id);
+        Task<ResponseDTO<IEnumerable<OrderDTO>>> GetOrdersAsync();
+        Task<ResponseDTO<IEnumerable<OrderDTO>>> GetOrdersAsync(OrderStatus orderStatus);
+        Task<ResponseDTO<IEnumerable<OrderDTO>>> GetOrdersAsync(string applicationUserId);
+        Task<ResponseDTO<IEnumerable<OrderDTO>>> GetOrdersAsync(DateTime startDate, DateTime endDate);
+        Task<ResponseDTO<IEnumerable<OrderDTO>>> CreateOrderAsync(OrderCreateDTO orderCreateDTO);
+        Task<ResponseDTO<NoContent>> UpdateOrderAsync(OrderUpdateDTO orderUpdateDTO);
+        Task<ResponseDTO<NoContent>> DeleteOrderAsync(int id);
+        
+       Task<ResponseDTO<IEnumerable<OrderDTO>>> GetOrdersBySellerIdAsync(string sellerId);
+
+    
+
+
+    }
+}
