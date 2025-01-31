a5115f0f9b6d90ef3b1ed0281ec536020e4c0d6c
Beyzaerdd <erdbeyza14@gmail.com>
2025-02-01 00:50:18 +0300

    ✅InvoicesController is completed

---
+++ Backend/ECommerce.API/Controllers/InvoicesController.cs
@@ -0,0 +1,37 @@
+﻿using ECommerce.Business.Abstract;
+using ECommerce.Business.Concrete;
+using ECommerce.Shared.DTOs.InvoiceDTOs;
+using ECommerce.Shared.DTOs.OrderDTOs;
+using ECommerce.Shared.Helpers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvoicesController : CustomControllerBase
+    {
+        private readonly IInvoiceService ınvoiceService;
+
+        public InvoicesController(IInvoiceService ınvoiceService)
+        {
+            this.ınvoiceService = ınvoiceService;
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateInvoice([FromBody] InvoiceCreateDTO invoiceCreateDTO)
+        {
+            var response = await ınvoiceService.CreateInvoiceAsync(invoiceCreateDTO);
+            return CreateResponse(response);
+        }
+
+        [HttpGet("getInvoiceby/{orderId}")]
+        public async Task<IActionResult> GetInvoiceByOrderId(int orderId)
+        {
+            var response = await ınvoiceService.GetInvoiceByOrderIdAsync(orderId);
+            return CreateResponse(response);
+        }
+
+        }
+}
