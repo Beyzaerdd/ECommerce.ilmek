2ce447fde80c6cd52622383afdd24888f9560238
Beyzaerdd <erdbeyza14@gmail.com>
2025-02-05 22:38:44 +0300

    ☑️Toastr and CookieAuthentication are added

--- Frontend/ECommerce.MVC/Program.cs
+++ Frontend/ECommerce.MVC/Program.cs
@@ -1,15 +1,60 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.DataProtection;
+using NToastNotify;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews()
+                .AddNToastNotifyToastr(new ToastrOptions
+                {
+                    ProgressBar = true,
+                    PositionClass =           ToastPositions.TopRight,
+                    CloseButton = true,
+                    TimeOut = 5000,
+                    ShowDuration = 1000,
+                    HideDuration = 1000,
+                    ShowEasing="swing",
+                    HideEasing="linear",
+                    ShowMethod="fadeIn",
+                    HideMethod="fadeOut"
+
+                });
+
+
+builder.Services.AddHttpClient("ECommerceAPI", client => client.BaseAddress = new Uri("http://localhost:1406/api/"));
+
+builder
+    .Services
+    .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.Cookie.Name = "ECommerce.Auth";
+        options.LoginPath = "/Auth/Login";
+        options.AccessDeniedPath = "/Auth/AccessDenied";
+        options.ExpireTimeSpan = TimeSpan.FromHours(1);
+        options.Cookie.HttpOnly = true;
+    });
+
+builder.Services.AddAuthorization();
+
+#region DataProtection
+builder.Services.AddDataProtection()
+    .PersistKeysToFileSystem(new DirectoryInfo(Path.Combine(builder.Environment.ContentRootPath, "keys")))
+    .SetApplicationName("ECommerce.MVC")
+    .SetDefaultKeyLifetime(TimeSpan.FromDays(14));
+
+builder.Services.AddDistributedMemoryCache();
+
+#endregion
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+
     app.UseHsts();
 }
 
@@ -19,6 +64,12 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.MapAreaControllerRoute(
+    name: "admin",
+    areaName: "Admin",
+    pattern: "Admin/{controller=Home}/{action=Index}/{id?}",
+    defaults: new { area = "Admin" });
 
 app.MapControllerRoute(
     name: "default",
