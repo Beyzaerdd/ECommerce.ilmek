5d8881711ff1cadd5fb9bfcf5dca0c7081c9d3c3
Beyzaerdd <erdbeyza14@gmail.com>
2025-02-06 23:09:29 +0300

    ☑️AuthModels added

---
+++ Frontend/ECommerce.MVC/Models/AuthModels/UserLoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.MVC.Models.AuthModels
+{
+    public class UserLoginModel
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
