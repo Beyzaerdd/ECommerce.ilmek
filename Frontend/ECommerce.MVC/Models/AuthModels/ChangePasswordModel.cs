5599a3001169a84e9b0e5828149fd478b4196a10
Beyzaerdd <erdbeyza14@gmail.com>
2025-02-06 23:09:06 +0300

    ☑️AuthModels added

---
+++ Frontend/ECommerce.MVC/Models/AuthModels/ChangePasswordModel.cs
@@ -0,0 +1,22 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+namespace ECommerce.MVC.Models.AuthModels
+{
+    public class ChangePasswordModel
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string CurrentPassword { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "Yeni şifre en az 6 karakter olmalıdır.")]
+        public string NewPassword { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "Yeni şifreler eşleşmiyor.")]
+        public string ConfirmNewPassword { get; set; }
+
+    }
+}
