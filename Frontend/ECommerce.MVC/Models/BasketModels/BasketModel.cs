22fc3b08d35c52b8d54d414b164c8dc83d37023c
Beyzaerdd <erdbeyza14@gmail.com>
2025-02-06 23:46:41 +0300

    🛸Model added to Basketmodel

---
+++ Frontend/ECommerce.MVC/Models/BasketModels/BasketModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.MVC.Models.BasketModels
+{
+    public class BasketModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string ApplicationUserId { get; set; }
+
+        public ApplicationUserModel ApplicationUser { get; set; }
+
+        public IEnumerable<BasketItemModel> BasketItems { get; set; } = new List<BasketItemModel>();
+
+        [StringLength(20, ErrorMessage = "Kupon kodu en fazla 20 karakter olabilir.")]
+        public string? CouponCode { get; set; }
+    }
+}
