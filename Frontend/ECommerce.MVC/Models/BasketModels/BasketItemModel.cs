7f67403514b9eb9904cc32a4d17ccba62e53a31e
Beyzaerdd <erdbeyza14@gmail.com>
2025-02-06 23:46:28 +0300

    🛸Model added to Basketmodel

---
+++ Frontend/ECommerce.MVC/Models/BasketModels/BasketItemModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.MVC.Models.BasketModels
+{
+    public class BasketItemModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int BasketId { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        public ProductModel Product { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Miktar en az 1 olmalıdır.")]
+        public int Quantity { get; set; }
+
+
+        public decimal OriginalPrice { get; set; }
+
+        public decimal DiscountedPrice { get; set; }
+    }
+}
