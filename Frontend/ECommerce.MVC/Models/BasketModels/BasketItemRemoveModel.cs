3ccef24a09447c2fd4abef5c44b4ecad83491438
Beyzaerdd <erdbeyza14@gmail.com>
2025-02-06 23:46:32 +0300

    🛸Model added to Basketmodel

---
+++ Frontend/ECommerce.MVC/Models/BasketModels/BasketItemRemoveModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.MVC.Models.BasketModels
+{
+    public class BasketItemRemoveModel
+    {
+        [Required]
+        public int BasketId { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+    }
+}
