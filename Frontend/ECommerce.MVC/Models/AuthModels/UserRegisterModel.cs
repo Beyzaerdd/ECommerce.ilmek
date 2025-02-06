1876d915a3f4878a49a87cac8b9a7deb4687f4a2
Beyzaerdd <erdbeyza14@gmail.com>
2025-02-06 23:09:34 +0300

    ☑️AuthModels added

---
+++ Frontend/ECommerce.MVC/Models/AuthModels/UserRegisterModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.MVC.Models.AuthModels
+{
+    public class UserRegisterModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "Şifre en az 6 karakter olmalıdır.")]
+        public string Password { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Şifreler eşleşmiyor.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required, Phone]
+        public string PhoneNumber { get; set; }
+    }
+}
