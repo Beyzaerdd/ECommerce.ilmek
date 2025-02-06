0e5b92321ad18679a69d10ecb21bed2ba97763ae
Beyzaerdd <erdbeyza14@gmail.com>
2025-02-06 23:46:14 +0300

    🛸Model added to Basketmodel

---
+++ Frontend/ECommerce.MVC/Models/BasketModels/BasketCreateModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.MVC.Models.BasketModels
+{
+    public class BasketCreateModel
+    {
+        [Required]
+        public string ApplicationUserId { get; set; }
+    }
+}
---
+++ Frontend/ECommerce.MVC/Models/BasketModels/BasketItemChangeQuantityModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.MVC.Models.BasketModels
+{
+    public class BasketItemChangeQuantityModel
+    {
+        [Required]
+        public int BasketItemId { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Miktar en az 1 olmalıdır.")]
+        public int Quantity { get; set; }
+    }
+}
