acc8eccf65181cf9e665d525004bbb1614585263
Beyzaerdd <erdbeyza14@gmail.com>
2025-02-09 00:01:43 +0300

    ☑️UserFavModels have been added

---
+++ Frontend/ECommerce.MVC/Models/UserFavModels/UserFavCreateModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ECommerce.MVC.Models.UserFavModels
+{
+    public class UserFavCreateModel
+    {
+        [JsonPropertyName("productId")]
+        public int? ProductId { get; set; }
+    }
+}
---
+++ Frontend/ECommerce.MVC/Models/UserFavModels/UserFavModel.cs
@@ -0,0 +1,24 @@
+﻿using ECommerce.MVC.Models.ProductModels;
+using ECommerce.Shared.DTOs.ProductDTOs;
+using System.Text.Json.Serialization;
+
+namespace ECommerce.MVC.Models.UserFavModels
+{
+    public class UserFavModel
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("applicationUserId")]
+        public string ApplicationUserId { get; set; }
+
+        [JsonPropertyName("applicationUserFullName")]
+        public string ApplicationUserFullName { get; set; }
+
+        [JsonPropertyName("productId")]
+        public int? ProductId { get; set; }
+
+        [JsonPropertyName("product")]
+        public ProductModel Product { get; set; }
+    }
+}
