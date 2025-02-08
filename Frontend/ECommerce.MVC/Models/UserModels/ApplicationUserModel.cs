2dcfc7640d35b4864b40d6b16bd1f1f5b96d1f85
Beyzaerdd <erdbeyza14@gmail.com>
2025-02-09 00:01:55 +0300

    ☑️UserModels have been added

---
+++ Frontend/ECommerce.MVC/Models/UserModels/ApplicationUserModel.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace ECommerce.MVC.Models.UserModels
+{
+    public class ApplicationUserModel
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [Required(ErrorMessage = "Ad zorunludur.")]
+        [MaxLength(100, ErrorMessage = "Ad 100 karakteri geçemez.")]
+        [JsonPropertyName("firstName")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Soyad zorunludur.")]
+        [MaxLength(100, ErrorMessage = "Soyad 100 karakteri geçemez.")]
+        [JsonPropertyName("lastName")]
+        public string LastName { get; set; }
+
+        [MaxLength(500, ErrorMessage = "Adres 500 karakteri geçemez.")]
+        [JsonPropertyName("address")]
+        public string Address { get; set; }
+
+        [MaxLength(100, ErrorMessage = "Şehir adı 100 karakteri geçemez.")]
+        [JsonPropertyName("city")]
+        public string City { get; set; }
+
+        [JsonPropertyName("storeName")]
+        public string? StoreName { get; set; }
+
+        [JsonPropertyName("isActive")]
+        public bool IsActive { get; set; }
+
+        [JsonPropertyName("isApproved")]
+        public bool IsApproved { get; set; }
+
+        [JsonPropertyName("identityNumber")]
+        public int IdentityNumber { get; set; }
+
+        [JsonPropertyName("dateOfBirth")]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required(ErrorMessage = "Kullanıcı adı zorunludur.")]
+        [JsonPropertyName("userName")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "E-posta zorunludur.")]
+        [EmailAddress(ErrorMessage = "Geçerli bir e-posta adresi girin.")]
+        [JsonPropertyName("email")]
+        public string Email { get; set; }
+
+        [JsonPropertyName("emailConfirmed")]
+        public bool EmailConfirmed { get; set; }
+
+        [Phone(ErrorMessage = "Geçerli bir telefon numarası girin.")]
+        [JsonPropertyName("phoneNumber")]
+        public string PhoneNumber { get; set; }
+    }
+}
