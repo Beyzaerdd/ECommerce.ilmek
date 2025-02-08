bb1231fbcd287c826a8e76dfc2f6156e64d691a3
Beyzaerdd <erdbeyza14@gmail.com>
2025-02-08 23:58:39 +0300

    ☑️ContactMessageModels have been added

---
+++ Frontend/ECommerce.MVC/Areas/Admin/Models/ContactMessageModels/ContactMessageModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ECommerce.MVC.Areas.Admin.Models.ContactMessageModels
+{
+    public class ContactMessageModel
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+        public string ApplicationUserId { get; set; }
+        public string ApplicationUserName { get; set; }
+    }
+}
