8fa1ccd5516ee2025bb826b40637220ae8c32160
Beyzaerdd <erdbeyza14@gmail.com>
2025-02-13 00:58:54 +0300

    🚀Navbar is added

---
+++ Frontend/ECommerce.MVC/ViewComponents/NavbarViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.MVC.ViewComponents
+{
+    public class NavbarViewComponent:ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
