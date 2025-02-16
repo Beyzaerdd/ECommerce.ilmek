cb5842c11f293a59e0573f2a14efdd725a0ed58e
Beyzaerdd <erdbeyza14@gmail.com>
2025-02-16 23:51:08 +0300

    ☑️IReviewService is added

---
+++ Frontend/ECommerce.MVC/Services/Abstract/IReviewService.cs
@@ -0,0 +1,19 @@
+﻿using ECommerce.MVC.Models.ReviewModels;
+using ECommerce.MVC.Views.Shared.ResponseViewModels;
+using ECommerce.Shared.DTOs.ResponseDTOs;
+using ECommerce.Shared.DTOs.ReviewDTOs;
+
+namespace ECommerce.MVC.Services.Abstract
+{
+    public interface IReviewService
+    {
+        Task<ResponseViewModel<NoContent>> AddReviewAsync(ReviewCreateModel reviewCreateModel);
+
+
+        Task<ResponseViewModel<IEnumerable<ReviewModel>>> GetReviewByProductIdAsync(int productId);
+
+        Task<ResponseViewModel<NoContent>> DeleteReviewAsync(int reviewId);
+
+        Task<ResponseViewModel<NoContent>> UpdateReviewAsync(ReviewUpdateModel reviewUpdateModel);
+    }
+}
