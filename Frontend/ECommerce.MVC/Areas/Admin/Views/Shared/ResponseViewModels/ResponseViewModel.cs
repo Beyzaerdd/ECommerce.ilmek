ab4009182b008273dbd59e76776693852ec8240e
Beyzaerdd <erdbeyza14@gmail.com>
2025-03-01 23:51:05 +0300

    🚀Created areas for Admin and Seller dashboards, structured folder organization

---
+++ Frontend/ECommerce.MVC/Areas/Admin/Views/Shared/ResponseViewModels/ResponseViewModel.cs
@@ -0,0 +1,33 @@
+﻿using ECommerce.Shared.DTOs.ResponseDTOs;
+using System.Text.Json.Serialization;
+
+namespace ECommerce.MVC.Areas.Admin.Views.Shared.ResponseViewModels
+{
+    public class ResponseViewModel<T>
+    {
+        [JsonPropertyName("data")]
+        public T? Data { get; set; }
+
+        [JsonPropertyName("errors")]
+        public List<ErrorViewModel>? Errors { get; set; }
+
+        [JsonPropertyName("success")]
+        public bool IsSucceeded { get; set; }
+
+        public static ResponseViewModel<T> FromApiResponse(ResponseDTO<T> apiResponse)
+        {
+            return new ResponseViewModel<T>
+            {
+                Data = apiResponse.Data,
+                Errors = apiResponse.Errors?.ConvertAll(e => new ErrorViewModel
+                {
+                    Message = e.Message,
+                    Code = e.Code,
+                    Target = e.Target
+                }),
+                IsSucceeded = apiResponse.IsSucceeded
+            };
+        }
+    }
+}
+
